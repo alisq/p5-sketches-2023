27ceeea9c5ab3f1036adf5b3c4a08c6e8b57c120
alisq <ali.shamas.qadeer@gmail.com>
2023-03-24 17:35:17 -0400

    code for dima

--- noise-example/sketch.js
+++ noise-example/sketch.js
@@ -1,6 +1,5 @@
 let c;
 let ourFont;
-let f = 0;
 let profs = ["ali","diane","craig","nancy","roderick","sadie","richard"]
 console.log(profs)
 let points;
@@ -14,7 +13,7 @@ function setup() {
     c = createCanvas(1000,1000)
 
     
-    
+    colorMode(HSB,255);
 
     textSize(100)
     textFont(ourFont);
@@ -37,15 +36,31 @@ function setup() {
 }
 
 function draw() {
-    colorMode(HSB,255);
-f=f+0.0001;
+
     
     
     
+    // noFill()
+
+
+    let ferb = map(mouseX, 0, 1000, 0, 255)
+    // strokeWeight(5)
+    fill(ferb,255,255)
+    // ellipse(mouseX,mouseY,50)
+    textSize(mouseY/10)
+    
+    
+    // let i = 0;
+    // while (i<profs.length) {
+    //     text(profs[i], 100,(100*i+100))
+
+    //     i=i+1;
+    // }
+
     
     for(let i=0; i<points.length; i++) {
-        fill(noise(f*points[i].x)*255,255,255)
-        ellipse(points[i].x+random(-5,5), points[i].y+random(-5,5),random(10,20))
+        fill(random(255),255,255)
+        rect(points[i].x+random(-5,5), points[i].y+random(-5,5),random(10,60))
         
     }
     
